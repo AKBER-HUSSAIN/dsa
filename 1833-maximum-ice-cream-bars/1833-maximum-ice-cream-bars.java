2a0ccafdca9d6b6ba75579ab82b67e99008ca170
AKBER HUSSAIN <132199862+AKBER-HUSSAIN@users.noreply.github.com>
2026-06-22 00:33:35 +0530

    Time: 4 ms (99.23%), Space: 82.4 MB (95.51%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,16 +1,63 @@
-
-import java.util.*;
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int n=0;
-        for(int i=0;i<costs.length;i++){
-            if(coins>=costs[i]){
-                coins -= costs[i];
-                n++;
+        int n = costs.length;
+        int maxVal = costs[0];
+        for(int x: costs) {
+            maxVal = Math.max(maxVal,x);
+        }
+
+        int[] freq = new int[maxVal+1];
+
+        for(int x: costs) {
+            freq[x] += 1;
+        }
+
+        int totalCost = 0;
+        int totalBought = 0;
+        int i = 0;
+        for(; i < freq.length; i++) {
+            totalCost += freq[i] * i;
+            if(totalCost > coins) {
+                totalCost -= freq[i] * i;
+                int availableCost = coins - totalCost;
+                if(availableCost > 0 && i > 0) {
+                    int canTake = availableCost/i;
+                    if(canTake > 0) {
+                        return totalBought + canTake;
+                    }
+                }
+                return totalBought;
+            } else {
+                totalBought += freq[i];
             }
         }
-        return n;
+
+        return totalBought;
     }
-    
+
+    public static int[] countingSort(int[] nums) {
+        int n = nums.length;
+        int maxVal = Integer.MIN_VALUE;
+        for(int x: nums) {
+            maxVal = Math.max(maxVal,x);
+        }
+
+        int[] countArray = new int[maxVal+1];
+
+        for (int num : nums) {
+            countArray[num] += 1;
+        }
+
+        for(int i = 1; i < countArray.length; i++) {
+            countArray[i] += countArray[i-1];
+        }
+
+        int[] sorted = new int[n];
+        for(int i = n-1; i >= 0; i--) {
+            countArray[nums[i]] -= 1;
+            sorted[countArray[nums[i]]]  = nums[i];
+        }
+
+        return sorted;
+    }
 }
