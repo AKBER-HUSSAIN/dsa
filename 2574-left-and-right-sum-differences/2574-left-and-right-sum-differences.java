96fb8cc5d1a4be4617e503d745291ad5e0ea2dc9
AKBER HUSSAIN <132199862+AKBER-HUSSAIN@users.noreply.github.com>
2026-07-31 00:37:33 +0530

    Time: 0 ms (100%), Space: 46.5 MB (73.86%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,22 +1,48 @@
+// class Solution {
+//     public int[] leftRightDifference(int[] nums) {
+//         int n = nums.length;
+//         int[] ans = new int[n];
+
+//         int totalSum = 0;
+//         for (int num : nums) {
+//             totalSum += num;
+//         }
+
+//         int leftSum = 0;
+//         int rightSum = totalSum;
+
+//         for (int i = 0; i < n; i++) {
+//             rightSum -= nums[i];
+//             ans[i] = Math.abs(leftSum - rightSum);
+//             leftSum += nums[i];
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
+        static{
+         for(int i=0;i<600;i++){
+           new Solution().leftRightDifference(new int[]{0,0});
+         }
+    }
     public int[] leftRightDifference(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        int arraySum = sum(nums);
+        int curtotal = 0;
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            curtotal += nums[i];
+            ans[i] = Math.abs((arraySum - curtotal) - (curtotal - nums[i]));
         }
-
-        int leftSum = 0;
-        int rightSum = totalSum;
-
-        for (int i = 0; i < n; i++) {
-            rightSum -= nums[i];
-            ans[i] = Math.abs(leftSum - rightSum);
-            leftSum += nums[i];
-        }
-
         return ans;
+    }
+
+    int sum(int[] nums){
+        int res = 0;
+        for(int num : nums){
+            res+=num;
+        }
+        return res;
     }
 }
