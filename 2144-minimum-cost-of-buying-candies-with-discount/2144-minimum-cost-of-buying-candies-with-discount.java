f8fdef930441ca4ac0085ad8580a23bcbded7e2e
AKBER HUSSAIN <132199862+AKBER-HUSSAIN@users.noreply.github.com>
2026-08-06 01:10:51 +0530

    Time: 1 ms (99.98%), Space: 44.1 MB (96.6%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,18 +1,47 @@
+// class Solution {
+//     public int minimumCost(int[] cost) {
+//         Arrays.sort(cost);
+//         int ans=0;
+//         int c=0;
+//         for(int i=cost.length-1;i>=0;i--){
+//             c++;
+//             if(c==3){
+//                 c=0;
+//                 continue;
+//             }
+//             ans+=cost[i];
+
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int minimumCost(int[] cost) {
-        Arrays.sort(cost);
-        int ans=0;
-        int c=0;
-        for(int i=cost.length-1;i>=0;i--){
-            c++;
-            if(c==3){
-                c=0;
-                continue;
-            }
-            ans+=cost[i];
-
+        int freq[]= new int[101];
+        int n=cost.length;
+        for(int i=0;i<cost.length;i++)
+        {
+            freq[cost[i]]++;
         }
-
+        int ans=0;
+        int bal=2;
+        for(int i=100;i>0;i--)
+        {
+            while(freq[i]>0)// highest element ki freq jb tk 0 na ho jaye 
+            {
+               freq[i]--;
+               if(bal==0)
+               {
+                 bal=2;// jb balance 0  ho jaye update balance and continue 
+                 continue;
+               }
+            bal--;
+            ans+=i;
+           }
+        }
         return ans;
     }
 }
