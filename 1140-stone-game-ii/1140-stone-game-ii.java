567cc4fd7200ca7e1ed277fa1cb53c6e9f87783b
AKBER HUSSAIN <132199862+AKBER-HUSSAIN@users.noreply.github.com>
2026-08-09 20:24:05 +0530

    Time: 2 ms (100%), Space: 44.2 MB (82.63%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,56 +1,46 @@
 class Solution {
-    int[][] dp;
-    int[] suffix;
-    int n;
+
+    private int[][] memo;
+    private int[] suffix;
+    private int n;
 
     public int stoneGameII(int[] piles) {
+
         n = piles.length;
 
-        // Suffix sum
         suffix = new int[n + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             suffix[i] = suffix[i + 1] + piles[i];
         }
 
-        dp = new int[n][n + 1];
+        memo = new int[n][n + 1];
 
-        for (int i = 0; i < n; i++) {
-            java.util.Arrays.fill(dp[i], -1);
-        }
-
-        return solve(0, 1, piles);
+        return dfs(0, 1);
     }
 
-    private int solve(int i, int M, int[] piles) {
-        if (i >= n) {
+    private int dfs(int i, int M) {
+
+        if (i >= n)
             return 0;
-        }
 
-        // Can take all remaining piles
-        if (i + 2 * M >= n) {
+        if (2 * M >= n - i)
             return suffix[i];
-        }
 
-        if (dp[i][M] != -1) {
-            return dp[i][M];
-        }
+        if (memo[i][M] != 0)
+            return memo[i][M];
 
-        int maxStones = 0;
+        int best = 0;
 
         for (int X = 1; X <= 2 * M; X++) {
 
-            int opponent = solve(
-                i + X,
-                Math.max(M, X),
-                piles
+            best = Math.max(
+                    best,
+                    suffix[i] - dfs(i + X, Math.max(M, X))
             );
-
-            int currentPlayer = suffix[i] - opponent;
-
-            maxStones = Math.max(maxStones, currentPlayer);
         }
 
-        return dp[i][M] = maxStones;
+        memo[i][M] = best;
+        return best;
     }
 }
