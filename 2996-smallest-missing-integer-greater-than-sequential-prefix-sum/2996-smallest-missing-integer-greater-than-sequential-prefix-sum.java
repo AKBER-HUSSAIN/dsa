c0260474390b58a75ea9ea55138c3d303b57c1e6
AKBER HUSSAIN <132199862+AKBER-HUSSAIN@users.noreply.github.com>
2026-08-12 01:45:01 +0530

    Time: 0 ms (100%), Space: 43.8 MB (92.31%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,22 +1,58 @@
+// class Solution {
+//     public int missingInteger(int[] nums) {
+//         int sum = nums[0];
+//         int i = 1;
+
+//         while (i < nums.length && nums[i] == nums[i - 1] + 1) {
+//             sum += nums[i];
+//             i++;
+//         }
+
+//         HashSet<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+
+//         while (set.contains(sum)) {
+//             sum++;
+//         }
+
+//         return sum;
+//     }
+// }
+
+
 class Solution {
     public int missingInteger(int[] nums) {
-        int sum = nums[0];
-        int i = 1;
-
-        while (i < nums.length && nums[i] == nums[i - 1] + 1) {
-            sum += nums[i];
-            i++;
+        int sum=nums[0];
+       for(int i=1;i<nums.length;i++)
+       {
+          if(nums[i]==nums[i-1]+1)
+          {
+            sum=sum+nums[i];
+          }
+          else
+          {
+            break;
+          }
+       }
+       int j=sum;
+       while(true)
+       {
+        boolean found=false;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==j)
+            {
+                found=true;
+                break;
+            }
         }
-
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+        if(!found)
+        {
+            return j;
         }
-
-        while (set.contains(sum)) {
-            sum++;
-        }
-
-        return sum;
+        j++;
+       }
     }
 }
